7b97e6727c36b86660ad8b70cf20406bc626c0b8
villainouskirby <69393156+villainouskirby@users.noreply.github.com>
2026-05-25 17:41:14 +0900

    player combined state 수정

--- Server/obj/Debug/net9.0/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net9.0/Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+586c9753884b9a4af2bd5eabbf682b842ef4156d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ae5d76f015b5da4f96c05ba72d0790241b56901")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
