eeb3cc62dd1b22d5560a1d81ed92998360862a49
Charen523 <144107013+Charen523@users.noreply.github.com>
2026-05-29 19:15:36 +0900

    feat : 재료 상호작용에 대한 반응

--- Assets/_Scripts/Entity/Object/IngredientReaction.cs
+++ Assets/_Scripts/Entity/Object/IngredientReaction.cs
@@ -15,41 +15,89 @@ public enum IngredientAction : byte
 public class IngredientReaction : MonoBehaviour
 {
     [SerializeField] private CatchableObj catchable;
+    public CatchableObj Catchable => catchable;
 
-    [Header("임시 메시")]
+    [Header("Components")]
     [SerializeField] private MeshFilter meshFilter;
+    [SerializeField] private Renderer targetRenderer;
+
+    [Header("Interact Gauge")]
+    private IngredientAction lastAction = IngredientAction.None;
+    private int curHP;
+    private int maxHP;
+    public float CurGuage => 1 - (float)curHP / maxHP;
+
+    #region 재료 변화 캐싱
     [SerializeField] private Mesh defaultMesh;
     [SerializeField] private Mesh cutMesh;
+    [SerializeField] private Material defaultMaterial;
+    [SerializeField] private Material cookedMaterial;
+    #endregion
 
-    public void Interact(IngredientAction action)
+    private void Awake()
+    {
+        maxHP = GetMaxHP();
+        curHP = maxHP;
+
+        //TODO : ResourceManager에서 Mesh, Shader 불러오기
+        //defaultMesh = ResourceManager.Instance.GetAsset<Mesh>("IngredientMesh");
+        //cutMesh = ResourceManager.Instance.GetAsset<Mesh>("CutIngredientMesh");
+        //defaultShader = ResourceManager.Instance.GetAsset<Shader>("DefaultShader");
+        //cookedShader = ResourceManager.Instance.GetAsset<Shader>("CookedShader");
+    }
+
+    public bool Interact(IngredientAction action, int dmg = 0)
     {
         if (IsActionBlocked(action))
         {
             Debug.Log("Action is blocked for this ingredient.");
-            return;
+            return false;
         }
 
-        switch (action)
+        if (lastAction != action)
         {
-            case IngredientAction.Cut:
-                Debug.Log("Cutting the ingredient.");
-                ApplyMesh(cutMesh);
-                break;
-            case IngredientAction.Grill:
-                Debug.Log("Grilling the ingredient.");
-                break;
-            case IngredientAction.Boil:
-                Debug.Log("Boiling the ingredient.");
-                break;
-            case IngredientAction.Cook:
-                Debug.Log("Cooking the ingredient.");
-                break;
-            default:
-                Debug.Log("Unknown action.");
-                break;
+            lastAction = action;
+            curHP = maxHP; // Reset HP when action changes
         }
-    }
+        else if (curHP <= 0)
+        {
+            Debug.Log("Action already completed.");
+            return false;
+        }
 
+        // Apply damage and check for completion
+        curHP = (int)MathF.Max(0, curHP - dmg);
+
+        //TODO : HP 감소에 따른 Mesh, Shader 변화 구현하기
+        //TODO : 완료된 플래그 표시해주기
+        if (curHP <= 0)
+        {
+            switch (action)
+            {
+                case IngredientAction.Cut:
+                    Debug.Log("Cutting Completed.");
+                    ApplyMesh(cutMesh); //TODO : dmg 반영하기
+                    break;
+                case IngredientAction.Grill:
+                    Debug.Log("Grilling Completed.");
+                    break;
+                case IngredientAction.Boil:
+                    Debug.Log("Boiling Completed.");
+                    break;
+                case IngredientAction.Cook:
+                    ApplyMaterial(cookedMaterial);
+                    break;
+                default:
+                    Debug.Log("Unknown action.");
+                    break;
+            }
+
+            return true; //Action 완료 시 Interact 호출 금지용
+        }
+
+        return false;
+    }
+    
     private bool IsActionBlocked(IngredientAction action)
     {
         if (catchable.Data is not Ingredient ingredient)
@@ -58,6 +106,7 @@ public class IngredientReaction : MonoBehaviour
         return ((IngredientAction)ingredient.conditionFlag & action) != 0;
     }
 
+    #region 재료 변화 적용
     private void ApplyMesh(Mesh mesh = null)
     {
         if (mesh == null)
@@ -68,4 +117,31 @@ public class IngredientReaction : MonoBehaviour
 
         meshFilter.mesh = mesh;
     }
+
+    private void ApplyMaterial(Material material = null)
+    {
+        //default shader
+         if (material == null)
+        {
+            targetRenderer.material = defaultMaterial;
+            return;
+        }
+
+        targetRenderer.material = material;
+    }
+    #endregion
+
+    #region helper methods
+    private int GetMaxHP()
+    {
+        if (catchable.Data is Ingredient ingredient)
+        {
+            IngredientStat ingredientStat = DataManager.Instance.GetIngredientStat().GetData(ingredient.statID);
+
+            return ingredientStat.hp;
+        }
+
+        return 0;
+    }
+    #endregion
 }
