0ea472481acbcd7e07eb2d2286bc9ab0b9cfca12
villainouskirby <69393156+villainouskirby@users.noreply.github.com>
2026-08-24 12:16:15 +0900

    플레이어 더티마스크 사용안되던 버그 수정

--- Protocol/obj/Debug/net9.0/Protocol.AssemblyInfo.cs
+++ Protocol/obj/Debug/net9.0/Protocol.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Protocol")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d513baf70fdfee2b186d71d664223c3d4804e036")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5488546d85efbffedcfae670aea768bcb368a99")]
 [assembly: System.Reflection.AssemblyProductAttribute("Protocol")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Protocol")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
