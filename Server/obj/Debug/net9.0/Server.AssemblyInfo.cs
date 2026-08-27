74361ba9d62574ddb592089d1d8e8ad338c6a0e8
villainouskirby <69393156+villainouskirby@users.noreply.github.com>
2026-08-27 17:51:20 +0900

    타이머 시간 단위 오류 수정

--- Server/obj/Debug/net9.0/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net9.0/Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef169a74d9f01d4b0aaf98152547c8f9d119ec56")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb3834afdcd4784c9f7890f57640152d52620447")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
